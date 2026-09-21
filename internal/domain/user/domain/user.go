56e802f1c808dd4355ddeab7cc59f9c081eadcee
Enock Yator <ekyator02@gmail.com>
2026-09-21 17:52:02 +0300

    chore: update domain specific files

--- internal/domain/user/domain/user.go
+++ internal/domain/user/domain/user.go
@@ -1,1 +1,32 @@
 package domain
+
+import "github.com/google/uuid"
+
+type UpsertUserParams struct {
+    Email           string
+    Name            string
+    AvatarUrl       *string
+    Provider        string
+    ProviderSubject string
+}
+
+type GetUserByProviderSubjectParams struct {
+    Provider        string
+    ProviderSubject string
+}
+
+type GetUserByEmailAndProviderParams struct {
+    Lower  string  // sqlc names it after the expression; verify in generated file
+    Provider string
+}
+
+type UpdateUserProfileParams struct {
+    ID        uuid.UUID
+    Name      string
+    AvatarUrl *string
+}
+
+type ListUsersParams struct {
+    Limit  int32
+    Offset int32
+}
