bb5cd88043a540400a273cff831a84b5f8045a77
Enock Yator <ekyator02@gmail.com>
2026-09-21 17:49:05 +0300

    chore: add session id hash utility

---
+++ internal/domain/auth/domain/hash.go
@@ -0,0 +1,11 @@
+package domain
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+func HashSessionID(raw string) string {
+    sum := sha256.Sum256([]byte(raw))
+    return hex.EncodeToString(sum[:])
+}
